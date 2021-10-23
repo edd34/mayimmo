a92940757d0ac252731fde78112640d6ccac24df
Eddine Omar <eddine.farid@hotmail.fr>
2021-10-23 11:48:57 +0300

    update listing model

--- components/listing/models.py
+++ components/listing/models.py
@@ -7,8 +7,8 @@ from components.helpers.estate_type import EstateType
 
 class Listing(models.Model):
     is_available = models.BooleanField()
-    date_available = models.BooleanField()
-    date_not_available = models.BooleanField()
+    available_since = models.DateField(null=True)
+    not_available_since = models.DateField(null=True)
     estate_type = models.TextField(choices=EstateType.choices)
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
     price = models.FloatField()
