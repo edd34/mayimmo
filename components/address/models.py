96975103099d2a5633e733356e23d50195d10767
Eddine Omar <eddine.farid@hotmail.fr>
2021-10-23 11:48:21 +0300

    update address model

--- components/address/models.py
+++ components/address/models.py
@@ -6,3 +6,5 @@ class Address(models.Model):
     address_2 = models.CharField(max_length=128, blank=True, null=True)
     city = models.CharField(max_length=64)
     zip_code = models.CharField(max_length=5)
+    longitude = models.FloatField(null=True)
+    latitude = models.FloatField(null=True)
