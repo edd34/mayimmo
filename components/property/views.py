2360b087345dabb2f306e8c89a12835477858556
Eddine Omar <eddine.farid@hotmail.fr>
2021-10-31 23:20:33 +0300

    implement filter and order

--- components/property/views.py
+++ components/property/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.parsers import JSONParser
-from components.property.serializers import PropertySerializer, PropertyFilterSerializer
+from components.property.serializers import PropertySerializer
 from components.property.models import Property
 from rest_framework.pagination import PageNumberPagination
 from django.db.models import Avg, Max, Min, Sum
@@ -22,6 +22,39 @@ def add_property(request):
 @api_view(['GET', 'OPTIONS'])
 def get_paginated_property(request):
     property = Property.objects.all()
+
+    min_area = request.query_params.get('min_area')
+    max_area = request.query_params.get('max_area')
+    min_price = request.query_params.get('min_price')
+    max_price = request.query_params.get('max_price')
+    min_price_per_area = request.query_params.get('min_price_per_area')
+    max_price_per_area = request.query_params.get('max_price_per_area')
+
+    order_field = request.query_params.get('order_by')
+    sort_order = request.query_params.get('sort_order')
+
+    # todo faire la gestion d'erreur
+    # todo deplacer dans une fonction et prendre en compte tous les champs
+    if min_area is not None:
+        property = property.filter(area__gte=min_area)
+    if max_area is not None:
+        property = property.filter(area__lte=max_area)
+    if min_price is not None:
+        property = property.filter(price__gte=min_price)
+    if max_price is not None:
+        property = property.filter(price__lte=max_price)
+    if min_price_per_area is not None:
+        property = property.filter(price_per_area__gte=min_price_per_area)
+    if max_price_per_area is not None:
+        property = property.filter(price_per_area__lte=max_price_per_area)
+
+    if order_field is not None and sort_order == "asc":
+        property = property.order_by(order_field)
+    elif order_field is not None and sort_order == "desc":
+        property = property.order_by("-"+order_field)
+    elif order_field is not None and sort_order not in ["asc", "desc"]:
+        return JsonResponse("Error in field sort order", status=status.HTTP_400_BAD_REQUEST)
+
     paginator.page_size = 10
     paginator.page_query_param = 'page'
     paginator.page_size_query_param = 'per_page'
